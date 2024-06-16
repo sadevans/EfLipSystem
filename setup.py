bd5279c90bc9cb8f167c2e855d5e5370e4a17983
sadevans <a.s.gorokhova@yandex.ru>
2024-06-16 14:25:27 +0300

    updated

--- setup.py
+++ setup.py
@@ -5,22 +5,19 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
     requirements = [req.rstrip("\n") for req in fh]
 
 setup(
+    name="backending",
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
     # packages=["backend", "pipelines", "model"],
     packages=find_packages(),
-    package_dir={"": "."},
+    package_dir={"pipelines": "./pipelines", 'model_zoo': './model_zoo'},
     include_package_data=True,
     package_data= {
         # all .dat files at any package depth
-        "model.EfLipReading.configs": ['**/*.yaml', '*.yaml', 'data/dataset/*.yaml', '**/**/*.yaml'],
-        "model.EfLipReading.model.labels": ['**/*.yaml', '*.yaml', '**/**/*.yaml', '**/**/*.yaml'],
-        "model.EfLipReading.data.detectors.mediapipe": ['**/*.yaml', '**/*.npy', 'detector/*.npy', '*.npy',],
         "pipelines.detectors.mediapipe": ['**/*.yaml', '**/*.npy', '*.npy'],
         "model_zoo": ['**/*.ckpt', '**/**/*.ckpt', '*.ckpt'],
-
         # into the data folder (being into a module) but w/o the init file
     },
     python_requires=">=3.8",
